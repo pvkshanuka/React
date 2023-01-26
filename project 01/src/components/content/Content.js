9cc3ba763b9decac32aded159549549100068ebf
pvkshanuka <pvkshanuka@gmail.com>
2023-01-26 23:39:27 +0530

    Creating Dynamic Components

--- project 01/src/components/content/Content.js
+++ project 01/src/components/content/Content.js
@@ -1,10 +1,13 @@
-import React from 'react'
-import './Content.css'
+import React from "react";
+import "./Content.css";
 
 export default function Content() {
+  const contentFirst = "Text, and ";
+  const content = "ipsum dolor sit amet consectetur adipisicing elit. Non porro quidem veritatis? Vel, minus omnis delectus excepturi maiores obcaecati, laborum aspernatur odio dignissimos enim, molestiae corrupti tempore repellendus amet eos.";
+
   return (
-        <div className="phara">
-            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Non porro quidem veritatis? Vel, minus omnis delectus excepturi maiores obcaecati, laborum aspernatur odio dignissimos enim, molestiae corrupti tempore repellendus amet eos.</p>
-        </div>
-  )
+    <div className="phara">
+      <p>{contentFirst} {content}</p>
+    </div>
+  );
 }

179d335af0a631ef7b47d580ac7f1e6aa0125b8c
pvkshanuka <pvkshanuka@gmail.com>
2023-01-28 23:37:00 +0530

    React Basics & Working With Components

---
+++ 01-starting-setup/src/App.js
@@ -0,0 +1,13 @@
+import ExpenseItem from "./components/ExpenseItem";
+
+function App() {
+  return (
+    <div>
+      <h2>Let's get started!</h2>
+      <p>This is visible</p>
+      <ExpenseItem/>
+    </div>
+  );
+}
+
+export default App;
---
+++ 01-starting-setup/src/components/ExpenseItem.js
@@ -0,0 +1,15 @@
+import './ExpenseItem.css'
+
+function ExpenseItem() {
+  return (
+    <div className="expense-item">
+      <div>January 28th 2023</div>
+      <div className='expense-item__description'>
+        <h2>Ranka Restaurant Payment</h2>
+        <div className='expense-item__price'>Rs2500</div>
+      </div>
+    </div>
+  );
+}
+
+export default ExpenseItem;

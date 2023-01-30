6117169f57835b1e0e463d1304c90bd6207786c0
pvkshanuka <pvkshanuka@gmail.com>
2023-01-31 00:00:46 +0530

    React State & Working with Events

--- 01-starting-setup/src/components/expenseItem/ExpenseItem.js
+++ 01-starting-setup/src/components/expenseItem/ExpenseItem.js
@@ -1,20 +1,28 @@
 import Card from "../UI/card/Card";
 import ExpenseDate from "../expenseDate/ExpenseDate";
 import "./ExpenseItem.css";
+import { useState } from "react";
 
 const ExpenseItem = (props) => {
 
+  const [title, setTitle] = useState(props.title);
+
+  
+
+  const clickHandler = () => {
+    setTitle('Updated Title!');
+  };
+
   return (
     <Card className="expense-item">
-      <ExpenseDate
-      date={props.date}
-      />
+      <ExpenseDate date={props.date} />
       <div className="expense-item__description">
-        <h2>{props.title}</h2>
+        <h2>{title}</h2>
         <div className="expense-item__price">Rs{props.amount}</div>
       </div>
+      <button onClick={clickHandler}>Change Title</button>
     </Card>
   );
-}
+};
 
 export default ExpenseItem;

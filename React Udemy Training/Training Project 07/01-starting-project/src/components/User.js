dd60e70c24a2b1476d2c067da3e5d67ad39a80a1
pvkshanuka <pvkshanuka@gmail.com>
2023-03-07 14:56:43 +0530

    Alternative Way Of Building Components: Class-based Components Completed

---
+++ React Udemy Training/Training Project 07/01-starting-project/src/components/User.js
@@ -0,0 +1,22 @@
+import { Component } from "react";
+import classes from "./User.module.css";
+
+class User extends Component {
+  componentWillUnmount() {
+    console.log("User will unmount!");
+  }
+
+  componentDidMount() {
+    console.log("User will mount!");
+  }
+
+  render() {
+    return <li className={classes.user}>{this.props.name}</li>;
+  }
+}
+
+// const User = (props) => {
+//   return <li className={classes.user}>{props.name}</li>;
+// };
+
+export default User;
